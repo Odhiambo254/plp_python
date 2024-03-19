3659b38b20323e61f1b741b8cb1c147218f3bf51
Odhiambo254 <odhiamboandrew76@gmail.com>
2024-03-19 11:20:15 +0300

    convert string to int

---
+++ 5-typecasting.py
@@ -0,0 +1,6 @@
+num1 = input("please enter your favourite number: ")
+num2 = input("now enter the most recent position you were: ")
+num1 = int(num1)
+num2 = int(num2)
+sum_total = num1 + num2
+print(f'{sum_total} is the sum of num1 and num2')
