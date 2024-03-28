ddf240d09476e739bbf98d26e767d8e975b00080
Odhiambo254 <odhiamboandrew76@gmail.com>
2024-03-28 08:04:18 +0300

    use the parameters and call assgned variable

---
+++ function_arguments.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+#function with parameters
+
+def add_nums(a, b):
+	answer = a + b
+	return answer
+
+#calling a function and assign the call to avariale
+total = add_nums(4, 9)
+print('Total : ', total)
