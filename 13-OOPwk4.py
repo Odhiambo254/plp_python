7d640caffd86d006e263236e70871e9d786f2008
Odhiambo254 <odhiamboandrew76@gmail.com>
2024-03-29 09:42:00 +0300

    creating classe and ther instances

---
+++ 13-OOPwk4.py
@@ -0,0 +1,13 @@
+class Person:
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+ #function to introduce 
+    def introduce(self):
+        print(f"hello, my name is {self.name}. i am {self.age} years old  and i am {self.gender}")
+            # cREATING AN INSTANCE 
+person1 = Person('ANDREW', 20, 'MALE')
+
+            #calling the function 
+person1.introduce()
