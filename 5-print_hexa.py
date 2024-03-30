d74ddf0ef216a14389d386b477d75039c96cb849
Odhiambo254 <odhiamboandrew76@gmail.com>
2024-03-30 21:05:52 +0300

    print hexadecimal

---
+++ 5-print_hexa.py
@@ -0,0 +1,5 @@
+#print hexadecimal numbers to 98.
+def print_hexadecimal(): # function to print hexadecimal
+    for i in range(0, 98): # give the range
+        print(hex(i)) # we use the inbuilt function hex to print hexadcimal .
+print_hexadecimal()
