fcd0e5586cc1ca7d05dee4c98b7dc6bb424355d2
Odhiambo254 <odhiamboandrew76@gmail.com>
2024-03-30 20:40:44 +0300

    print_aplhabet in lowercase

---
+++ 3-print_alphabet.py
@@ -0,0 +1,4 @@
+def lower_alphabet():
+    for i in range(97, 123):# this uses the ascii values that correspond to each letter
+        print(chr(i), end="")# the chr converts the i into characte value.
+lower_alphabet()# we now call the function.
